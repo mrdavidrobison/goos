ea904c187e6b1c4c66cfcdc3731f9a748141ce2a
david robison <mrdavidrobison@gmail.com>
2019-08-13 12:30:11 -0600

    add delete route

--- routes/api/items.js
+++ routes/api/items.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 })
 
 // @route POST api/items
-// @desc  Create All items 
+// @desc  Create an item
 // @access Public
 
 router.post('/', (req, res) => {
@@ -25,7 +25,23 @@ router.post('/', (req, res) => {
     name: req.body.name
   })
   newItem.save()
-  .then(item => res.json(item))
+    .then(item => res.json(item))
 })
+
+// @route DELETE api/items
+// @desc  Delete an item
+// @access Public
+
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove()
+      .then(() => res.json({
+        success: true
+      })))
+    .catch(err => res.status(404).json({
+      success: false
+    }))
+})
+
 
 module.exports = router
